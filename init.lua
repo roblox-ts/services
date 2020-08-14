65c7ae81c3b5dcebf5eada702ceb154bc54a2d88
Charlie West-Toebe <38671683+Hoidberg@users.noreply.github.com>
2020-08-14 16:19:36 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,7 +1,11 @@
 return setmetatable({}, {
-    __index = function(self, serviceName)
-        local service = game:GetService(serviceName);
-        self[serviceName] = service;
-        return service;
-    end;
+	__index = function(self, serviceName)
+		if game:FindService(serviceName) ~= nil then
+			local service = game:GetService(serviceName)
+			self[serviceName] = service
+			return service
+		else
+			error("'" .. tostring(serviceName) .. "'" .. "is not a valid Service name")
+		end
+	end
 })

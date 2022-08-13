b74701129df83a5ecc0fb5182c46d54d0be28911
osyrisrblx <osyrisrblx@gmail.com>
2022-08-13 16:38:02 -0400

    Update plugin.d.ts

--- plugin.d.ts
+++ plugin.d.ts
@@ -1,9 +1,27 @@
 /// <reference types="@rbxts/types/plugin" />
 
 declare module "@rbxts/services" {
-	// @ts-expect-error
+	// @ts-ignore
 	export * from "@rbxts/services/index";
+}
 
-	export const Selection: Selection;
+declare module "@rbxts/services" {
+	export const ChangeHistoryService: ChangeHistoryService;
 	export const CoreGui: CoreGui;
+	export const DebuggerManager: DebuggerManager;
+	export const NetworkClient: NetworkClient;
+	export const NetworkServer: NetworkServer;
+	export const NetworkSettings: NetworkSettings;
+	export const PackageService: PackageService;
+	export const PluginDebugService: PluginDebugService;
+	export const PluginGuiService: PluginGuiService;
+	export const RenderSettings: RenderSettings;
+	export const RobloxPluginGuiService: RobloxPluginGuiService;
+	export const Selection: Selection;
+	export const Studio: Studio;
+	export const StudioData: StudioData;
+	export const StudioService: StudioService;
+	export const TaskScheduler: TaskScheduler;
+	export const TestService: TestService;
+	export const VersionControlService: VersionControlService;
 }

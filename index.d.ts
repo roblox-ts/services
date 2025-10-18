a39c0e82ab6e15425c87066605066acfdd58d4d7
false <31769632+askfalse@users.noreply.github.com>
2025-10-17 19:06:23 -0500

    Add AvatarCreationService (#93)
    https://create.roblox.com/docs/en-us/reference/engine/classes/AvatarCreationService

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 export declare const AnalyticsService: AnalyticsService;
 export declare const AnimationClipProvider: AnimationClipProvider;
 export declare const AssetService: AssetService;
+export declare const AvatarCreationService: AvatarCreationService;
 export declare const AvatarEditorService: AvatarEditorService;
 export declare const BadgeService: BadgeService;
 export declare const CaptureService: CaptureService;
